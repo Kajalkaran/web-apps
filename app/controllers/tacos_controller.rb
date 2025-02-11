bbf3bb24d0f5d7ba0f1dd0d159f5a219f9d7e17b
Kajalkaran Shah <drkajal.sh@gmail.com>
2025-02-11 21:28:36 +0000

    lab dice

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,6 +1,7 @@
 class TacosController < ApplicationController
   def index
     #some code here
-    render :inline=> "<h1>Hello, world!</h1>"
+    #render :inline=> "<h1>Hello, world!</h1>"
+    render :template =>"tacos/index"
   end
 end
